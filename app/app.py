77f6c049fceb8e21bc41cc2dca4970d46ee362bf
Appari N V M Sai Kalyan <119591957+kalyanappari@users.noreply.github.com>
2025-04-14 23:43:20 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -38,7 +38,7 @@ model = Pipeline([
 model.fit(X_train, y_train)
 
 # Streamlit UI
-st.title("🌍 Indian Language Classification")
+st.title("🌍 Indian Language Classification Model")
 st.write("Enter a text snippet/Sentence/word, and the model will predict the language!")
 
 # User input
